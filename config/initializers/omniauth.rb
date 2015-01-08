27556c5268cb427932df13efb8c08c2f81f2165d
Joonhwan Lee <joonhwan@gmail.com>
2015-01-09 00:10:59 +0900

    Remove Old Version

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret_key'], scope: "email,publish_stream"
-end
